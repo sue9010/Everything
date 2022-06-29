4cbaad64d522f68a597a3edfbf94dc18c9513022
sue9010 <sue@coxcamera.com>
2022-06-29 11:28:31 +0900

    Update 1_job.py
    불필요한 주석들 및 코드 삭제

--- 4_nomad/1_웹스크래퍼/1_job.py
+++ 4_nomad/1_웹스크래퍼/1_job.py
@@ -1,10 +1,7 @@
 from bs4 import BeautifulSoup 
 import requests
 
-# indeed_result = requests.get("https://kr.indeed.com/jobs?q=python&l=%EC%84%9C%EC%9A%B8&limit=50&radius=25&start=500&vjk=e03c3db92d5c1291")
 indeed_result = requests.get("https://indeed.com/jobs?q=python&limit=50")
-
-# print(indeed_result.text)
 
 indeed_soup = BeautifulSoup(indeed_result.text, 'html.parser')
 
@@ -15,10 +12,5 @@ pages = pagination.find_all('a')
 spans = []
 for page in pages:
     spans.append(page.find("span"))
-    # print(page.find("span"))
 
 print(spans[:-1])
-
-# print(pages)
-
-
