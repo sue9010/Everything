b764d2774d3d3b99b77c0635db85959bd578ac56
sue9010 <sue@coxcamera.com>
2022-06-29 15:24:03 +0900

    extract_job_title function 화
    job title text만 추출하는 function 제작

--- 4_nomad/1_웹스크래퍼/indeed.py
+++ 4_nomad/1_웹스크래퍼/indeed.py
@@ -1,3 +1,4 @@
+from typing import final
 from bs4 import BeautifulSoup 
 import requests
 LIMIT = 50
@@ -6,13 +7,16 @@ URL = f"https://indeed.com/jobs?q=python&limit={LIMIT}"
 def extract_indeed_pages():
     result = requests.get(URL)
     soup = BeautifulSoup(result.text, 'html.parser')
-    pagination = soup.find("div", {"class":"pagination"})
-    links = pagination.find_all('a')
-    pages = []
-    for link in links[0:-1]:
-        pages.append(int(link.string))
-    max_page = pages[-1]
-    return max_page
+    try:
+        pagination = soup.find("div", {"class":"pagination"})
+        links = pagination.find_all('a')
+        pages = []
+        for link in links[0:-1]:
+            pages.append(int(link.string))
+        max_page = pages[-1]
+        return max_page
+    except AttributeError as e:
+        pass
 
 def extract_indeed_jobs(last_page):
     jobs =[]
@@ -20,12 +24,9 @@ def extract_indeed_jobs(last_page):
     result = requests.get(f"{URL}&start={0*LIMIT}")
     soup = BeautifulSoup(result.text, 'html.parser')
     results = soup.find_all('div', {"class":"job_seen_beacon"})
+    spans =[]
     for result in results:
-        title = result.find("h2", {"class":"jobTitle"})
-        anchor =title.find("a", {"class":"jcs-JobTitle"})
-        span = anchor.find("span", {"class":"title"})0
-        print(span)
-        # anchor = title.find("span")["title"]
-        # print(anchor)
-
+        title = result.find("h2", {"class":"jobTitle"}).find("a", {"class":"jcs-JobTitle"}).find("span")
+        final_title = title['title']
+        print(final_title)
     return jobs
