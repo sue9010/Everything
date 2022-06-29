31b5a2b41e030fea61866763343e04eaa0a2fac0
sue9010 <sue@coxcamera.com>
2022-06-29 13:05:09 +0900

    last indeed page를 function화
    function = last_indeed_page 추가

--- 4_nomad/1_웹스크래퍼/indeed.py
+++ 4_nomad/1_웹스크래퍼/indeed.py
@@ -1,10 +1,10 @@
 from bs4 import BeautifulSoup 
 import requests
-
-INDEED_URL = "https://indeed.com/jobs?q=python&limit=50"
+LIMIT = 50
+URL = f"https://indeed.com/jobs?q=python&limit={LIMIT}"
 
 def extract_indeed_pages():
-    result = requests.get(INDEED_URL)
+    result = requests.get(URL)
 
     soup = BeautifulSoup(result.text, 'html.parser')
 
@@ -19,3 +19,7 @@ def extract_indeed_pages():
     max_page = pages[-1]
 
     return max_page
+
+def extract_indeed_jobs(last_page):
+    for page in range(last_page):
+        print(f"&start={page*LIMIT}")
