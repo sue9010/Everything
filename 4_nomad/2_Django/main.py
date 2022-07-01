9d2680def75d753f97c7b86b012566f5cfc37231
sue9010 <sue@coxcamera.com>
2022-07-01 09:44:18 +0900

    push

---
+++ 4_nomad/2_Django/main.py
@@ -0,0 +1,30 @@
+class Car():
+
+    def __init__(self, *args, **kwargs):
+        self.wheels = 4
+        self.doors = 4
+        self.windows = 4
+        self.seats = 4
+        self.color = kwargs.get("color","black")
+        self.price = kwargs.get("price","20USD")
+
+    def __str__(self):
+        return f"Car with {self.wheels} wheels"
+
+class Convertable(Car):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def take_off(self):
+        return "taking off"
+
+    def __str__(self):
+        return f"Car with no roof"
+
+class Something(Convertable):
+    pass
+
+porche = Convertable(color="Green",price="40USD")
+porche.take_off()
+print(porche.color)
